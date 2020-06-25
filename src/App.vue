6be0d8207a6ef44729ddd6d1a757e9acfabc13c2
kmyr poureatesam <kmyr.rp@gmail.com>
2020-06-25 09:43:27 +0430

    create a loader for site

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <div>
-    <router-view></router-view>
+    <router-view class="application"></router-view>
+    <div class="spinner-border" role="status" id="loader">
+      <span class="sr-only">Loading...</span>
+    </div>
   </div>
 </template>
 
 <script>
+import $ from "jquery";
 export default {
   data() {
     return {
       result: null
     };
+  },
+  created() {
+    $(window).on("load", function() {
+      setTimeout(function() {
+        $(".application").show();
+        $("#loader").hide();
+      }, 800);
+    });
   }
 };
 </script>
 
-<style></style>
+<style>
+#loader {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -50px;
+  margin-left: -50px;
+  width: 100px;
+  height: 100px;
+}
+</style>
